2f8a2ed51a83fae3ba1ca6318ced7bbf52b532e8
Caio Vieira <caiocesarav0208@gmail.com>
2021-06-22 11:26:14 -0300

    learn context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-
+import { createContext } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
 
 import { NewRoom } from "./pages/NewRoom";
 
+export const TestContext = createContext('');
+
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms/new" component={NewRoom} />
+      <TestContext.Provider value={'teste'}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/new" component={NewRoom} />
+      </TestContext.Provider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import { useHistory } from 'react-router-dom';
 
 import { auth, firebase } from '../services/firebase'
@@ -7,13 +8,15 @@ import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
 import { Button } from '../components/Button'
+import { TestContext } from '../App'
+
 
 import '../styles/auth.scss';
 
 export function Home() {
     const history = useHistory();
 
-
+    const value = useContext(TestContext)
 
 
     function handleCreateRoom() {
@@ -36,6 +39,7 @@ export function Home() {
                 <p>Tire duvidas da sua audiência em tempo real</p>
             </aside>
             <main>
+                <h1>{value}</h1>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <Button onClick={handleCreateRoom} className="create-room">
