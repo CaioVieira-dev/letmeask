698f4b5f8f6e568b31ca7b94e8e8ce61b8c9a22f
Caio Vieira <caiocesarav0208@gmail.com>
2021-06-22 09:48:03 -0300

    create home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <h1>hello world</h1>
+    <Home />
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import App from './App';
 
 import './services/firebase';
 
+import './styles/global.scss';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
