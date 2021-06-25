b5666ae1f87e03a3431a07221abf31cc121f4ce8
Caio Vieira <caiocesarav0208@gmail.com>
2021-06-25 10:03:34 -0300

    highlight questions and firebase hosting added

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react'
+import cx from 'classnames'
+
 import './styles.scss'
 
 type QuestionProps = {
@@ -8,14 +10,21 @@ type QuestionProps = {
         avatar: string;
     };
     children?: ReactNode;
+    isAnswered?: boolean;
+    isHighlighted?: boolean;
 }
 
 export function Question({
     content,
     author,
-    children }: QuestionProps) {
+    children,
+    isAnswered = false,
+    isHighlighted = false }: QuestionProps) {
     return (
-        <div className="question">
+        <div
+            className={cx('question',
+                { answered: isAnswered },
+                { highlighted: isHighlighted && !isAnswered })}>
             <p>{content}</p>
             <footer>
                 <div className="user-info">
--- src/pages/Room/AdminRoom/index.tsx
+++ src/pages/Room/AdminRoom/index.tsx
@@ -8,6 +8,8 @@ import { RoomCode } from 'components/RoomCode';
 
 import logoImg from 'assets/images/logo.svg';
 import deleteImg from 'assets/images/delete.svg'
+import checkImg from 'assets/images/check.svg'
+import answerImg from 'assets/images/answer.svg'
 
 import '../styles.scss';
 //import { FormEvent, useState } from 'react';
@@ -41,7 +43,13 @@ export function AdminRoom() {
         }
     }
 
+    async function handleCheckQuestionAsAnswered(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({ isAnswered: true })
+    }
 
+    async function handleHighlightQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({ isHighlighted: true })
+    }
 
     return (
         <div id="page-room">
@@ -69,7 +77,23 @@ export function AdminRoom() {
                             <Question
                                 content={question.content}
                                 author={question.author}
+                                isHighlighted={question.isHighlighted}
+                                isAnswered={question.isAnswered}
                                 key={question.id}>
+                                {!question.isAnswered && (
+                                    <>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleCheckQuestionAsAnswered(question.id)}>
+                                            <img src={checkImg} alt="Marcar pergunta como respondida" />
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleHighlightQuestion(question.id)}>
+                                            <img src={answerImg} alt="Dar destaque a pergunta" />
+                                        </button>
+                                    </>
+                                )}
                                 <button
                                     type="button"
                                     onClick={() => handleDeleteQuestion(question.id)}>
